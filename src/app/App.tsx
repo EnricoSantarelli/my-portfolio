b9352786840fae2b4ede45e6fd7f63b370a7b7a4
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-10-25 23:26:28 -0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,69 @@
+import { createContext, useState } from "react";
+import Header from "./view/components/Header/Header";
+
+export type ThemeContextType = {
+  theme: string,
+  toggleTheme: () => void,
+  backgroundColor: string,
+  containerBackgroundColor: string,
+  boldFontColor: string,
+  thinFontColor: string,
+}
+
+type ThemeInfo = {
+  backgroundColor: string;
+  containerBackgroundColor: string;
+  boldFontColor: string;
+  thinFontColor: string;
+}
+
+const themes: Record<string, ThemeInfo> = {
+  "light": {
+    backgroundColor: "var(--light-background)",
+    containerBackgroundColor: "var(--light-container)",
+    boldFontColor: "var(--light-bold-font)",
+    thinFontColor: "var(--light-thin-font)",
+  },
+  "dark": {
+    backgroundColor: "var(--dark-background)",
+    containerBackgroundColor: "var(--dark-container)",
+    boldFontColor: "var(--dark-bold-font)",
+    thinFontColor: "var(--dark-thin-font)",
+  },
+};
+
+export const ThemeContext = createContext<ThemeContextType | null>(null);
+
+function App() {
+
+  //dark and light mode logic
+  const [theme, setTheme] = useState<string>("light");
+  const [backgroundColor, setBackgroundColor] = useState<string>(themes.light.backgroundColor);
+  const [containerBackgroundColor, setContainerBackgroundColor] = useState<string>(themes.light.containerBackgroundColor);
+  const [boldFontColor, setBoldFontColor] = useState<string>(themes.light.boldFontColor);
+  const [thinFontColor, setThinFontColor] = useState<string>(themes.light.thinFontColor);
+
+  const toggleTheme = () => {
+    const newTheme: string = theme === "light" ? "dark" : "light";
+    const selectedThemeInfo = themes[newTheme];
+
+    setBackgroundColor(selectedThemeInfo.backgroundColor);
+    setContainerBackgroundColor(selectedThemeInfo.containerBackgroundColor);
+    setBoldFontColor(selectedThemeInfo.boldFontColor);
+    setThinFontColor(selectedThemeInfo.thinFontColor);
+    setTheme(newTheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, backgroundColor, containerBackgroundColor, boldFontColor, thinFontColor }}>
+      <div className="App" style={{ background: backgroundColor }}>
+        <div className="container">
+          <Header />
+
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/app/view/components/Header/Header.tsx
@@ -0,0 +1,55 @@
+import myLogo from "../../../assets/my_logo.svg";
+import { PiListBold, PiSunBold, PiMoonBold } from "react-icons/pi";
+import "./Header.css";
+import { useContext, useEffect, useRef, useState } from "react";
+import { ThemeContext, ThemeContextType } from "../../../App";
+
+export default function Header() {
+
+    // close mobile navbar logic
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+    const navbarRef = useRef<HTMLElement>(null);
+    const divIconRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        // close mobile navbar when clicking outside of it
+        document.addEventListener("mousedown", (event) => {
+            if (!navbarRef.current?.contains(event.target as Node) && !divIconRef.current?.contains(event.target as Node)) {
+                setIsNavbarOpen(false);
+            }
+        });
+    });
+
+    //dark and light mode logic
+    const { toggleTheme, boldFontColor, containerBackgroundColor, thinFontColor } = useContext(ThemeContext) as ThemeContextType;
+
+    return (
+        <header id="Header">
+            <div className="headerLeft">
+                <img className="myLogo" src={myLogo} alt="Logo Enrico Santarelli" />
+                <h1 className="myName">Enrico Santarelli</h1>
+                <div ref={divIconRef} style={{ transform: isNavbarOpen ? "scaleX(1.5)" : "scaleX(1)", color: boldFontColor }} className="divIcon" onClick={() => setIsNavbarOpen(!isNavbarOpen)}>
+                    <PiListBold />
+                </div>
+                <nav ref={navbarRef} style={{ width: isNavbarOpen ? "144px" : "0px", backgroundColor: containerBackgroundColor }} className="navbarColumn">
+                    <ul className="navbarColumnList">
+                        <li className="navbarColumnItens"><a href="" className="navbarColumnLinks" style={{ color: thinFontColor }}>home</a></li>
+                        <li className="navbarColumnItens"><a href="" className="navbarColumnLinks" style={{ color: thinFontColor }}>sobre</a></li>
+                        <li className="navbarColumnItens"><a href="" className="navbarColumnLinks" style={{ color: thinFontColor }}>portfólio</a></li>
+                        <li className="navbarColumnItens"><a href="" className="navbarColumnLinks" style={{ color: thinFontColor }}>contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <nav ref={navbarRef} style={{ width: isNavbarOpen ? "144px" : "0px" }} className="navbarHeader">
+                <ul className="navbarHeaderList">
+                    <li className="navbarHeaderItens"><a href="" className="navbarHeaderLinks" style={{ color: thinFontColor }}>home</a></li>
+                    <li className="navbarHeaderItens"><a href="" className="navbarHeaderLinks" style={{ color: thinFontColor }}>sobre</a></li>
+                    <li className="navbarHeaderItens"><a href="" className="navbarHeaderLinks" style={{ color: thinFontColor }}>portfólio</a></li>
+                    <li className="navbarHeaderItens"><a href="" className="navbarHeaderLinks" style={{ color: thinFontColor }}>contato</a></li>
+                </ul>
+            </nav>
+            <div className="modeIcon" style={{ color: boldFontColor }} onClick={toggleTheme}>
+                <PiSunBold />
+            </div>
+        </header >
+    );
+}
